13abcb27bb836f5df558e006e9fb8dd29756f921
seobin Lee <znzltjqls@naver.com>
2019-04-30 09:39:15 +0900

    hello.py modified on local repository

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-
-python("hello world")
+//for command line git tutorial
+//local repository modification
+print("hello world")
+print("tell your world")
+print("tell his world")
+print("tell her world")
